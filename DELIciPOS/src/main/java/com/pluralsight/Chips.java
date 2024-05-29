7ddc0e0105d7fb28871a745d4703bbbfe3898a83
Aaron J. Rhodes <aaronj.rhodes@aarons-mbp.mynetworksettings.com>
2024-05-29 09:05:05 -0700

    chips

--- DELIciPOS/src/main/java/com/pluralsight/Chips.java
+++ DELIciPOS/src/main/java/com/pluralsight/Chips.java
@@ -1,5 +1,22 @@
 package com.pluralsight;
 
-public class Chips
-{
+class Chips implements Orderable {
+    private String id;
+    private String type;
+    private double price;
+
+    public Chips(String type, double price) {
+        this.id = UUID.randomUUID().toString();
+        this.type = type;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public double calculateCost() {
+        return price;
+    }
 }
