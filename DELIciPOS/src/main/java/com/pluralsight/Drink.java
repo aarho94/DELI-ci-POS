5ce8578eebc8f8abcf63b455d86fcabf43b0bb13
Aaron J. Rhodes <aaronj.rhodes@aarons-mbp.mynetworksettings.com>
2024-05-29 09:04:32 -0700

    drink

--- DELIciPOS/src/main/java/com/pluralsight/Drink.java
+++ DELIciPOS/src/main/java/com/pluralsight/Drink.java
@@ -1,5 +1,24 @@
 package com.pluralsight;
 
-public class Drink
-{
+class Drink implements Orderable {
+    private String id;
+    private String size; // small, medium, large
+    private String flavor;
+    private double price;
+
+    public Drink(String size, String flavor, double price) {
+        this.id = UUID.randomUUID().toString();
+        this.size = size;
+        this.flavor = flavor;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public double calculateCost() {
+        return price;
+    }
 }
