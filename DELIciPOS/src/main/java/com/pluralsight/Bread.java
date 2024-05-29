a3e5f9ce980026b065e426f53a1b38a21c1ad815
Aaron J. Rhodes <aaronj.rhodes@aarons-mbp.mynetworksettings.com>
2024-05-29 09:05:47 -0700

    bread

--- DELIciPOS/src/main/java/com/pluralsight/Bread.java
+++ DELIciPOS/src/main/java/com/pluralsight/Bread.java
@@ -1,5 +1,22 @@
 package com.pluralsight;
 
-public class Bread
-{
+class Bread {
+    private String type;
+
+    public Bread(String type) {
+        this.type = type;
+    }
+
+    public double getPrice(int sandwichSize) {
+        switch (sandwichSize) {
+            case 4:
+                return 1.0;
+            case 8:
+                return 1.5;
+            case 12:
+                return 2.0;
+            default:
+                return 0.0;
+        }
+    }
 }
