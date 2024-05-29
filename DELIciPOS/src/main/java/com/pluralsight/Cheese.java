1e2cebfe5d9fc8135aa346b1b24d4bd115c29b95
Aaron J. Rhodes <aaronj.rhodes@aarons-mbp.mynetworksettings.com>
2024-05-29 09:07:12 -0700

    cheese

---
+++ DELIciPOS/src/main/java/com/pluralsight/Cheese.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+class Cheese extends Topping {
+    public Cheese(String name) {
+        super(name, "premium");
+    }
+
+    @Override
+    public double getPrice(int sandwichSize) {
+        switch (sandwichSize) {
+            case 4:
+                return 1.0;
+            case 8:
+                return 1.5;
+            case 12:
+                return 2.0;
+            default:
+                return 0.0;
+        }
+    }
+}
